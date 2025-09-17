bef6de80d9ec35ac6a53401c20a7c33402503119
Morfeo <gastongomez2014@hotmail.com>
2025-09-17 17:24:59 -0300

    Se instalaron las dependencias para el proyecto

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+		<p className="text-green-500"> Hola Mundo </p>
+    </>
+  )
+}
+
+export default App
